104f5d3788c5bda934877045a97c14b33f05d6e9
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-03-02 17:13:27 +0100

    Fix path

--- src/ui/SelectionTable.js
+++ src/ui/SelectionTable.js
@@ -5,7 +5,7 @@ import { useTable, useRowSelect } from 'react-table';
 import { Checkbox } from './Checkbox';
 import { Box, getBoxProps } from './Box';
 import { uniqueId } from 'lodash';
-import { getValueFromTheme } from '../theme';
+import { getValueFromTheme } from './theme';
 import { Inline } from './Inline';
 import { Button, ButtonVariants } from './Button';
 import { Text } from './Text';
